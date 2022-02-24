9aa577185b5fd197eda7c77d64742860652fa2e7
smallMote <luckyoung@certbase.com>
2022-02-24 23:54:11 +0800

    del:test

--- test/index.js
+++ test/index.js
@@ -2,21 +2,7 @@
  * @Descripttion: your project
  * @version: 0.0.0
  * @Author: Minyoung
- * @Date: 2022-02-21 17:18:24
+ * @Date: 2022-02-21 22:56:34
  * @LastEditors: Minyoung
- * @LastEditTime: 2022-02-21 17:20:21
+ * @LastEditTime: 2022-02-24 23:53:54
  */
-let arr = [
-  'certumstore.com',
-  'app.certbase.com',
-  'app.certbase.com1',
-  'certumstore.com',
-  'certumstore.com',
-  'certumstore.com',
-  'certumstore.com',
-  'app.certbase.com',
-  'app.certbase.com1'
-]
-
-arr = new Set(arr)
-console.log(arr)
